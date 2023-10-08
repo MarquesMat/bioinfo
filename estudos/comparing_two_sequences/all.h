896cb2d8e9504bd352d0a08ef33135e3a2f7b7ca
MarquesMat <90152478+MarquesMat@users.noreply.github.com>
2023-10-08 14:05:55 -0300

    Update all.h
    Adicionado comentários às funções.
    Única parte alterada do código foi a variável global k, a qual pode ser mudada a qualquer momento dependendo do tamanho das sequências a serem inseridas.

--- estudos/comparing_two_sequences/all.h
+++ estudos/comparing_two_sequences/all.h
@@ -4,11 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define k 10   // Tamanho máximo das sequências
+#define k 50   // Tamanho máximo das sequências
 #define g (-2) // Penalidade por espaço vazio (gap)
 extern char align_s[k * 2];
 extern char align_t[k * 2];
 
+// Essa estrutura facilita passar todos os valores dela nas funções recursivas
 struct auxiliar
 {
     int m, n;
@@ -16,10 +17,12 @@ struct auxiliar
     char s[k], t[k];
 };
 
-int tam_seq(char *seq);
-int par(char a, char b);
-int max(int x, int y, int z);
-void print_seq(char *t, int n);
-void print_matrix(char *s, char *t, int m, int n, int **a);
+int choose_comparation(); // O usuário escolhe o tipo de comparação
+int tam_seq(char *seq); // Retorna o tamanho de uma sequência
+int par(char a, char b); // Retorn 1 se a == b ou 0 se a != b
+int max(int x, int y, int z); // Retorna o maior valor entre x, y e z
+void print_seq(char *t, int n); // Imprime a sequência t na parte superior da matriz
+void print_matrix(char *s, char *t, int m, int n, int **a); // Imprime a matriz de similaridade, incluindo a sequência s na sua lateral
+void print_alignments(char *align, int len); // Imprime as sequências alinhadas
 
 #endif // ALL_H
